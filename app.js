0786c04165a2f208f037ff23071ec22097d943a9
Andy <andy@Andys-MBP-2.lan1>
2018-11-20 17:13:09 -0500

    Add authorization and flash

--- app.js
+++ app.js
@@ -2,26 +2,27 @@ var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
+    flash       = require("connect-flash"),
     passport    = require("passport"),
     LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"),
     Campground  = require("./models/campground"),
     Comment     = require("./models/comment"),
-    User        = require("./models/user"),
-    seedDB      = require("./seeds")
-    
+    User        = require("./models/user")
+    //seedDB      = require("./seeds")
+   
+        
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-
-mongoose.connect("mongodb://localhost:27017/yelp_camp_v6", { useNewUrlParser: true });
-
+mongoose.connect("mongodb://localhost/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-//seedDB();
-
-
+app.use(methodOverride("_method"));
+app.use(flash());
+// seedDB(); //seed the database
 
 // PASSPORT CONFIGURATION
 app.use(require("express-session")({
@@ -37,15 +38,14 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next){
    res.locals.currentUser = req.user;
+   res.locals.error = req.flash("error");
+   res.locals.success = req.flash("success");
    next();
 });
-
 
 app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
-
-
 
 
 app.listen(3000, function(){
--- seeds.js
+++ seeds.js
@@ -49,10 +49,10 @@ function seedDB(){
                             }
                         });
                 }
-              });
-           });
-     }); 
-    // //add a few comments
+            });
+        });
+    }); 
+    //add a few comments
 }
 
 module.exports = seedDB;
