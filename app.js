77817451582300c6a0f9ddd0d16c42b03e4c5c1b
Andy <andy@Andys-MBP-2.lan1>
2018-11-21 16:30:32 -0500

    update dburl

--- app.js
+++ app.js
@@ -15,10 +15,9 @@ var express     = require("express"),
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
-    
-mongoose.connect("mongodb://andy:avabk31@ds029801.mlab.com:29801/yelpcamphsin");
-//mongoose.connect("mongodb://localhost/yelpcamp");
-//mongodb://andy:avabk31@ds029801.mlab.com:29801/yelpcamphsin
+
+mongoose.connect(process.env.DATABASEURL);
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -50,9 +49,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-// app.listen(3000, function(){
-//     console.log("The YelpCamp Server Has Started!");
-// });
 
 app.listen(process.env.PORT || 3000, function(){
    console.log("The YelpCamp Server Has Started!");
